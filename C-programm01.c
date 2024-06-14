7aa34f7c7885dd83e8693877b4e4b73c0cb109f4
koplercito <kopler201x@gmail.com>
2024-06-14 03:43:48 -0600

    Float ussage

--- C-programm01.c
+++ C-programm01.c
@@ -2,7 +2,7 @@
 
 main()
 {
-    int fahr, celsius;
+    float fahr, celsius;
     int lower, upper, step;
 
     lower = 0;
@@ -11,8 +11,8 @@ main()
 
     fahr = lower;
     while (fahr <= upper) {
-        celsius = 5 * (fahr-32) / 9;
-        printf("%d\t%d\n", fahr, celsius);
+        celsius = (5.0/9.0) * (fahr-32.0);
+        printf("%3.1f %6.1f\n", fahr, celsius);
         fahr = fahr + step;
     }
 }
